130e8507354ecadd272bd096d008bd04a3e09241
Carine TOdmia <ctodmia@gmail.com>
2015-09-27 16:20:17 -0700

    stringified 80% done. gotta figure out how to pass objects

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,84 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
   // your code goes here
+  	//JSON.stringify({});                  // '{}'
+	//JSON.stringify(true);                // 'true'
+	//JSON.stringify('foo');               // '"foo"'
+	//JSON.stringify([1, 'false', false]); // '[1,"false",false]'
+	//JSON.stringify({ x: 5 });            // '{"x":5}'
+
+	//i have no idea what this has to do with recursion.
+	//so basically we want that whatever we pass as an argument to stringifyJSON  
+	//it should wrap quotes around that parameter. 
+	//so stringifyJSON([1,2,3]) ==> '[1,2,3]'
+
+	//we got a wonderful clue. look at your fixtures.js file. you will notice that there i an variable 
+	//called stringifiableObjects and this is an ARRAY! this array is what our spec is using to inspect
+	//our code. with that being said we know we need to iterate over this array and make sure 
+	//that each one get stringified to pass all all test. 
+
+	//strategy: for example make 9 ==> '9' think about how to iterate over the array stringifiableObjects.
+	//lets also try refactoring the code by making a separate function that adds quotes around obj. 
+	//(or is that what this stringifyJSON is suppose to do?)
+
+
+
+var stringifyJSON = function(obj) {
+
+
+//lets start of by writing some of our base cases. We know that there are some unstringifiable and they are
+// functions and undefined. 
+
+ if (obj === null) {
+    return "null";
+  }
+
+  if (obj === undefined) { 
+  	return;
+  } 
+  if (obj.constructor === Function) { 
+  	return; 
+  }
+
+  // strings
+  if (obj.constructor === String) {
+    return '"' + obj + '"';
+  }
+
+   if (obj.constructor === Number || obj.constructor === Boolean) {
+	return "" + obj; 
+  }
+
+  // now lets see if we can iterate through our array. 
+  if (Array.isArray(obj)) {
+    if (obj.length) {
+      var stringArrays = [];
+//this is where the recursion happens. 
+      for (var i = 0; i < obj.length; i++) {
+        stringArrays.push(stringifyJSON(obj[i])); 
+      }
+
+      return '[' + stringArrays.join(",") + ']';
+    } else {
+      return '[]';
+    }
+  }
+
+ 
+if (obj.constructor === Object) {
+	if (obj.length) {
+		var stringObj = [];
+		for (var key in obj) {
+			
+			stringObj.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));	
+		}
+
+		return '{' + stringObj.join(',') + '}';
+	}
+	return '{}';
+}
+
 };
+
+
